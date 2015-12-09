fafc830698cb4b5d61c36765457c27ea99f87fbe
Victor LISTRAT <vlistrat@e2r2p12.42.fr>
2015-12-09 12:08:13 +0100

    header

--- ft_struct.c
+++ ft_struct.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_struct.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlistrat <vlistrat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/09 12:05:25 by vlistrat          #+#    #+#             */
+/*   Updated: 2015/12/09 12:05:50 by vlistrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fillit.h"
 
 int	ft_ret_size(char ***ret)
@@ -29,35 +41,5 @@ int	**ft_fill_coord(int i, int j, int k, int **coord)
 {
 	coord[i][0] = j;
 	coord[i][1] = k;
-	return (coord);
-}
-
-int	**ft_struct(char ***ret)
-{
-	int	i;
-	int	j;
-	int	k;
-	int	**coord;
-
-	i = 0;
-	coord = ft_coord(ret);
-	while (ret[i])
-	{
-		j = 0;
-		while (j < 4)
-		{
-			k = 0;
-			while (k < 4 && ret[i][j][k] != '#')
-				k++;
-			if (ret[i][j][k] == '#')
-			{
-				coord = ft_fill_coord(i, j, k, coord);
-				coord[i][2] = ft_is_tetri(ret[i], j, k);
-				i++;
-				j = 4;
-			}
-			j++;
-		}
-	}
 	return (coord);
 }
