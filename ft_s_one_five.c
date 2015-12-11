f02d6a0d1df9bddb31bb7d2fb5af0fb3fc57e8b1
Victor LISTRAT <vlistrat@e2r2p7.42.fr>
2015-12-11 16:52:50 +0100

    struct

--- ft_s_one_five.c
+++ ft_s_one_five.c
@@ -1,28 +1,27 @@
 #include "fillit.h"
 
-t_flist		ft_struct_one(void)
+t_flist		ft_struct_one(t_list *new)
 {
-	t_flist		 *new;
-
 	new = (t_flist*)malloc(sizeof(*new));
 	if (new == NULL)
 		return (NULL);
-	maillon->x = 0;
-	maillon->y = 0;
-	maillon->x1 = 0;
-	maillon->y1 = 1;
-	maillon->x2 = 1;
-	maillon->y2 = 0;
-	maillon->x3 = 1;
-	maillon->y3 = 1;
+	new->x = 0;
+	new->y = 0;
+	new->x1 = 0;
+	new->y1 = 1;
+	new->x2 = 1;
+	new->y2 = 0;
+	new->x3 = 1;
+	new->y3 = 1;
 	new->forme = "##\n##";
 	new->prev = NULL;
 	new->next = NULL;
-	return (maillon);
+	return (new);
 }
 
 void		ft_struct_two(t_flist *maillon)
 {
+	t_flist		*new;
 	maillon->x = 0;
 	maillon->y = 0;
 	maillon->x1 = 1;
