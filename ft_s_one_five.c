6837ecfe525e1dd55f5b12181a3f8a405e094c3d
Victor LISTRAT <vlistrat@e2r2p7.42.fr>
2015-12-11 17:06:51 +0100

    struct one five

--- ft_s_one_five.c
+++ ft_s_one_five.c
@@ -19,51 +19,74 @@ t_flist		ft_struct_one(t_list *new)
 	return (new);
 }
 
-void		ft_struct_two(t_flist *maillon)
+void		ft_struct_two(t_flist *new)
 {
-	t_flist		*new;
-	maillon->x = 0;
-	maillon->y = 0;
-	maillon->x1 = 1;
-	maillon->y1 = 0;
-	maillon->x2 = 2;
-	maillon->y2 = 0;
-	maillon->x3 = 3;
-	maillon->y3 = 0;
+	new = (t_flist*)malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+	new->x = 0;
+	new->y = 0;
+	new->x1 = 1;
+	new->y1 = 0;
+	new->x2 = 2;
+	new->y2 = 0;
+	new->x3 = 3;
+	new->y3 = 0;
+	new->forme = "####";
+	new->prev = NULL;
+	new->next = NULL;
 }
 
-void		ft_struct_three(t_flist *maillon)
+void		ft_struct_three(t_flist *new)
 {
-	maillon->x = 0;
-	maillon->y = 0;
-	maillon->x1 = 0;
-	maillon->y1 = 1;
-	maillon->x2 = 0;
-	maillon->y2 = 2;
-	maillon->x3 = 0;
-	maillon->y3 = 3;
+	new = (t_flist*)malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+	new->x = 0;
+	new->y = 0;
+	new->x1 = 0;
+	new->y1 = 1;
+	new->x2 = 0;
+	new->y2 = 2;
+	new->x3 = 0;
+	new->y3 = 3;
+	new->forme = "#\n#\n#\n#";
+	new->prev = NULL;
+	new->next = NULL;
 }
 
-void		ft_struct_four(t_flist *maillon)
+void		ft_struct_four(t_flist *new)
 {
-	maillon->x = 0;
-	maillon->y = 0;
-	maillon->x1 = 1;
-	maillon->y1 = 0;
-	maillon->x2 = 1;
-	maillon->y2 = 1;
-	maillon->x3 = 2;
-	maillon->y3 = 1;
+	new = (t_flist*)malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+	new->x = 0;
+	new->y = 0;
+	new->x1 = 1;
+	new->y1 = 0;
+	new->x2 = 1;
+	new->y2 = 1;
+	new->x3 = 2;
+	new->y3 = 1;
+	new->forme = "##.\n.##";
+	new->prev = NULL;
+	new->next = NULL;
 }
 
-void		ft_struct_five(t_flist *maillon)
+void		ft_struct_five(t_flist *new)
 {
-	maillon->x = 1;
-	maillon->y = 0;
-	maillon->x1 = 0;
-	maillon->y1 = 1;
-	maillon->x2 = -1;
-	maillon->y2 = 1;
-	maillon->x3 = -1;
-	maillon->y3 = 2;
+	new = (t_flist*)malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+	new->x = 1;
+	new->y = 0;
+	new->x1 = 0;
+	new->y1 = 1;
+	new->x2 = -1;
+	new->y2 = 1;
+	new->x3 = -1;
+	new->y3 = 2;
+	new->forme = ".#\n##\n#.";
+	new->prev = NULL;
+	new->next = NULL;
 }
