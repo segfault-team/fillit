fe66ab0b9011850eb9342b960770e1c2d4a0c51e
Victor LISTRAT <vlistrat@e2r1p7.42.fr>
2015-12-14 12:34:54 +0100

    error

--- ft_error.c
+++ ft_error.c
@@ -6,15 +6,15 @@
 /*   By: vlistrat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/12 14:19:50 by vlistrat          #+#    #+#             */
-/*   Updated: 2015/12/12 14:25:00 by vlistrat         ###   ########.fr       */
+/*   Updated: 2015/12/14 12:34:24 by vlistrat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //TEST
 
-int		ft_error(int ft, int a)
+int		ft_error(int ft)
 {
-	if ((a == 1 && ft != 2) || (a == 2 && ft != 1))
+	if (ft < 0)
 	{
 		ft_putendl_fd("error", 1);
 		return (-1);
